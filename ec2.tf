2ead216f8d58ba5a05b78e373fdb4574ef163d4e
Atchaya T <atchaya.602@gmail.com>
2026-02-14 23:14:26 +0530

    Initial commit

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c1fe732b5494dc14" # Amazon Linux 2 AMI (update per region)
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public.id
+  security_groups = [aws_security_group.web_sg.id]
+  key_name      = "devops_keypair"
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
