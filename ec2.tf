e184818fbe54517043c19b57c60beef607cc7de1
Atchaya T <atchaya.602@gmail.com>
2026-02-14 23:48:28 +0530

    ec2 update

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c1fe732b5494dc14" # Amazon Linux 2 AMI (update per region)
+  ami           = "ami-0317b0f0a0144b137" # Amazon Linux 2 AMI (update per region)
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public.id
   security_groups = [aws_security_group.web_sg.id]
